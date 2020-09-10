47f3493530197ff1074561c264712068a198e607
unknown <Deep Mehta>
2020-09-10 13:22:15 +0530

    Initial commit

---
+++ src/com/travel/App.java
@@ -0,0 +1,14 @@
+
+package com.travel;
+
+       public class App {
+    public static void main (String[] args)throws InterruptedException
+           
+    {
+        Splash s = new Splash();
+        MainMenu mm=new MainMenu();
+        
+    }
+            
+    
+}
